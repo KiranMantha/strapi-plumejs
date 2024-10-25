4c2c40b1dad8a9d2d093b5c63155d709d8239c92
KiranMantha <kiranv.mantha@gmail.com>
2024-10-25 18:01:20 +0530

    added serve and build scripts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       plugins: [
         visualizer({
           title: 'Plumejs example repo',
-          open: true
+          open: false
         })
       ]
     }
